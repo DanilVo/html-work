81ced1a7a813de77182dbc8078705c98a6cb9060
DanilV <dani736@gmail.com>
2022-06-07 06:07:09 +0300

    working on slides v.2

---
+++ js/slides.js
@@ -0,0 +1,6 @@
+//querySelector= checking for class with specific name in (hooks)
+let classbtnRight = document.querySelector(".btnRight")
+//querySelectorALL= checking for all classes with name in (hooks)
+let varslides = document.querySelectorAll('img')
+
+let i = 0;
